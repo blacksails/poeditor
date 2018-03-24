0be0d8fcb03b5209fc1b1f249a1a2e8682ef0701
Benjamin Nørgaard <b@noergaard.dk>
2018-03-24 21:54:56 +0100

    Move http logic behind interface in order to enable testing

--- poeditor.go
+++ poeditor.go
@@ -16,12 +16,20 @@ import (
 
 // POEditor is the main type used to interact with POEditor
 type POEditor struct {
+	poster poster
+}
+
+type poster interface {
+	post(string, map[string]string, map[string]io.Reader, interface{}) error
+}
+
+type poEditorPoster struct {
 	apiToken string
 }
 
 // New returns a new POEditor given a POEditor API Token
 func New(apiToken string) *POEditor {
-	return &POEditor{apiToken: apiToken}
+	return &POEditor{poster: poEditorPoster{apiToken: apiToken}}
 }
 
 // Project returns a Project with the given id
@@ -30,12 +38,16 @@ func (poe *POEditor) Project(id int) *Project {
 }
 
 func (poe *POEditor) post(endpoint string, fields map[string]string, files map[string]io.Reader, res interface{}) error {
+	return poe.poster.post(endpoint, fields, files, res)
+}
+
+func (p poEditorPoster) post(endpoint string, fields map[string]string, files map[string]io.Reader, res interface{}) error {
 	// Initiate fields if nil
 	if fields == nil {
 		fields = make(map[string]string)
 	}
 	// Set API Token
-	fields["api_token"] = poe.apiToken
+	fields["api_token"] = p.apiToken
 	// Initiate multipart writer
 	var body bytes.Buffer
 	writer := multipart.NewWriter(&body)
